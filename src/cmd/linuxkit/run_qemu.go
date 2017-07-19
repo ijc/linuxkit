af89b2a6a8990f2a7de7dfcd30d185b08e1a5a7c
Ian Campbell <ijc@docker.com>
2017-07-19 11:48:07 +0100

    Move ports processing to a separate file
    I hope to use this in the hyperkit runner too.
    
    Signed-off-by: Ian Campbell <ijc@docker.com>

--- src/cmd/linuxkit/run_qemu.go
+++ src/cmd/linuxkit/run_qemu.go
@@ -476,67 +476,6 @@ func discoverBackend(config QemuConfig) QemuConfig {
 	return config
 }
 
-type multipleFlag []string
-
-type publishedPorts struct {
-	guest    int
-	host     int
-	protocol string
-}
-
-func (f *multipleFlag) String() string {
-	return "A multiple flag is a type of flag that can be repeated any number of times"
-}
-
-func (f *multipleFlag) Set(value string) error {
-	*f = append(*f, value)
-	return nil
-}
-
-func splitPublish(publish string) (publishedPorts, error) {
-	p := publishedPorts{}
-	slice := strings.Split(publish, ":")
-
-	if len(slice) < 2 {
-		return p, fmt.Errorf("Unable to parse the ports to be published, should be in format <host>:<guest> or <host>:<guest>/<tcp|udp>")
-	}
-
-	hostPort, err := strconv.Atoi(slice[0])
-
-	if err != nil {
-		return p, fmt.Errorf("The provided hostPort can't be converted to int")
-	}
-
-	right := strings.Split(slice[1], "/")
-
-	protocol := "tcp"
-	if len(right) == 2 {
-		protocol = strings.TrimSpace(strings.ToLower(right[1]))
-	}
-
-	if protocol != "tcp" && protocol != "udp" {
-		return p, fmt.Errorf("Provided protocol is not valid, valid options are: udp and tcp")
-	}
-	guestPort, err := strconv.Atoi(right[0])
-
-	if err != nil {
-		return p, fmt.Errorf("The provided guestPort can't be converted to int")
-	}
-
-	if hostPort < 1 || hostPort > 65535 {
-		return p, fmt.Errorf("Invalid hostPort: %d", hostPort)
-	}
-
-	if guestPort < 1 || guestPort > 65535 {
-		return p, fmt.Errorf("Invalid guestPort: %d", guestPort)
-	}
-
-	p.guest = guestPort
-	p.host = hostPort
-	p.protocol = protocol
-	return p, nil
-}
-
 func buildQemuForwardings(publishFlags multipleFlag, containerized bool) (string, error) {
 	forwardings := "user"
 	for _, publish := range publishFlags {
